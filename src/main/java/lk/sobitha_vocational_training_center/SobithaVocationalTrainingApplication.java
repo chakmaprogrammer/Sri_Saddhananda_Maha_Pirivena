115e0684404f882f4db3b784040fb397bb470a90
lalithk90 <asakahatapitiya@gmail.com>
2021-05-17 21:22:16 +0530

    initial commit was done

---
+++ src/main/java/lk/sobitha_vocational_training_center/SobithaVocationalTrainingApplication.java
@@ -0,0 +1,13 @@
+package lk.sobitha_vocational_training_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobithaVocationalTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SobithaVocationalTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/sobitha_vocational_training_center/SobithaVocationalTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sobitha_vocational_training_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SobithaVocationalTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
